bc90e0dd72f9cbb0546ad1836f242c075ae98898
Arthur Santos <88942381+arthurjs92@users.noreply.github.com>
2023-04-11 21:26:48 -0300

    aplication properties configurados

--- back-end/gateway/src/main/java/com/mcheck/gateway/gateway/GatewayApplication.java
+++ back-end/gateway/src/main/java/com/mcheck/gateway/gateway/GatewayApplication.java
@@ -1,8 +1,12 @@
-package com.mcheck.gateway;
+package com.mcheck.gateway.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
 
--- back-end/gateway/src/test/java/com/mcheck/gateway/gateway/GatewayApplicationTests.java
+++ back-end/gateway/src/test/java/com/mcheck/gateway/gateway/GatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mcheck.gateway;
+package com.mcheck.gateway.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
